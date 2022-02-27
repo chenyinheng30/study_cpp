64c7ec648ec6869ce31bc7d9170868a2be3bed86
chenyinheng30 <chenyinheng30@outlook.com>
2022-02-27 17:31:55 +0800

    vector学习

--- Hello_word.cpp
+++ Hello_word.cpp
@@ -1,9 +1,29 @@
 #include<iostream>
-#include<cstring>
-#include<memory>
-#include"cpp_tools.h"
+#include<vector>
+struct my_class
+{
+    int x,y,z;
+    my_class(int x,int y,int z):x(x),y(y),z(z){}
+    my_class(const my_class& mc)
+    {
+        std::cout<<"copy!!!"<<std::endl;
+    }
+    ~my_class()
+    {
+        std::cout<<"destructor"<<std::endl;
+    }
+    friend std::ostream& operator<<(std::ostream& os,const my_class& mc);
+};
+std::ostream& operator<<(std::ostream& stream,const my_class& mc)
+{
+    stream<<"("<<mc.x<<","<<mc.y<<","<<mc.z<<")";
+    return stream;
+}
 int main()
 {
-    
+    std::vector<my_class> v;
+    v.push_back(my_class(1,2,3));
+    v.push_back(my_class(4,5,6));
+    v.push_back(my_class(7,8,9));
     return 0;
 }
