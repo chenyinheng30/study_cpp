a43188b6491e9a93077bb6dd89d30a787583e1a2
chenyinheng30 <chenyinheng30@outlook.com>
2022-03-01 21:19:18 +0800

    glfw3静态库使用,编译时要加上Gdi32的库，太草了

--- Hello_word.cpp
+++ Hello_word.cpp
@@ -1,34 +1,10 @@
 #include<iostream>
-#include<vector>
 #include"cpp_tools.h"
-struct my_class
-{
-    int x,y,z;
-    my_class(int x,int y,int z):x(x),y(y),z(z){}
-    my_class(const my_class& mc):x(mc.x),y(mc.y),z(mc.z)
-    {
-        std::cout<<"copy!!!"<<*this<<std::endl;
-    }
-    ~my_class()
-    {
-        std::cout<<"destructor:"<<*this<<std::endl;
-    }
-    friend std::ostream& operator<<(std::ostream& os,const my_class& mc);
-};
-std::ostream& operator<<(std::ostream& stream,const my_class& mc)
-{
-    stream<<"("<<mc.x<<","<<mc.y<<","<<mc.z<<")";
-    return stream;
-}
+#include"include\GLFW\glfw3.h"
+
 int main()
 {
-    std::vector<my_class> v;
-    v.reserve(3);//每次申请3个空间
-    v.emplace_back(1,2,3);
-    v.emplace_back(4,5,6);
-    v.emplace_back(7,8,9);
-
-    v.emplace_back(10,11,12);
-    SIMPLE_LOG("return");
+    int init_flag=glfwInit();
+    std::cout<<init_flag<<std::endl;
     return 0;
 }
